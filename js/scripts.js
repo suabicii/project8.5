34c8693ad5d0b62e7d8fa1b293a114ae5e146ab0
Michal Slabik <michal.slabik3@wp.pl>
2017-04-27 01:22:52 +0200

    Add js/scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+var a = prompt('Podaj podstawę trójkąta'),
+	h = prompt('Podaj wysokość trójkąta'),
+	triangleArea = a*h/2;
+
+console.log('Pole trójkąta o podstawie a: ' + a + ' i wysokości h: ' + h + ' wynosi: ' + triangleArea);
